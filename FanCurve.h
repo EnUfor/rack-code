2e764f0e8142134da6532f7a2ff2ab8b14acbc89
EnUfor <EnUfor@gmail.com>
2020-10-28 23:38:26 -0400

    Introduction of getFanSpeed and linearInterpolate

--- FanCurve.h
+++ FanCurve.h
@@ -1,6 +1,7 @@
 // temp vs fan %
 const short fanCurve[][2] = {
+    { 25 ,  10 },
     { 30 ,  20 },
     { 40 ,  60 },
     { 43 , 100 },
-}
+};
--- app.ino
+++ app.ino
@@ -4,7 +4,8 @@
 #include <Wire.h>
 #include <cactus_io_BME280_I2C.h>
 #include <CircularBuffer.h>
-#include "config.h" // Turns out you need to use quotes for relative imports (2 hrs wasted)
+#include "config.h"     // Turns out you need to use quotes for relative imports (2 hrs wasted)
+#include "fanCurve.h"   // Also turns out the includes' first character has to be lower case (10 min wasted)
 
 unsigned long MQTT_sensor_timer;
 unsigned long MQTT_reconnect_timer;
@@ -41,6 +42,7 @@ public:
     // ~Sensor();
     BME280_I2C sensor;
     // CircularBuffer<double, 20> history;
+    // CircularBuffer<double, 20> hi;
     double temp;
     double humidity;
     int fanSpeed = 1024;
@@ -99,7 +101,7 @@ public:
     // NodeMCU ESP8266 pinout: SCL = 5 (D1) SDA = 4 (D2)
     Sensor inlet = Sensor(0x76);
     Sensor outlet = Sensor(0x77);
-    CircularBuffer<double, 20> YUWORKHERE;
+    // CircularBuffer<double, 20> YUWORKHERE;
 
     /**
      * Read sensor values
@@ -113,8 +115,8 @@ public:
      * Print sensor values
     **/
     void printSensors() {
-        printer("Inlet", inlet);
-        printer("Outlet", outlet);
+        // printer("Inlet", inlet);
+        // printer("Outlet", outlet);
         Serial.println("\n");
     }
 };
@@ -287,4 +289,67 @@ void connect_MQTT() {
         Serial.println((String)"Failed to connect to MQTT with state: " + client.state());
     }
     Serial.println('\n');
+}
+
+/**
+ * IS THIS SECTION BELOW NECESSARY IF WE'RE GOING TO UTILIZE PID CONTROL??? PROBABLY NOT
+**/
+
+// Adapted from Heater.ino
+/**
+ * Gets the desired fan speed based on a fan curve
+ * @return {double} y - Interpolated fan speed (0% - 100%).
+ */ 
+float getFanSpeed() {
+    float temp = 0;
+
+    // This section would utilize a filter or rolling average to get desired
+    // fan speed based on a fan speed v. temperature curve speciifed in fanCurve.h
+
+    // 
+    // Since our array contains entities of type 'short' (16 bits or 2 bytes each),
+    // sizeof(temptable) represents the size of the entire array (256 bytes).
+    // sizeof(temptable[0]) represents the size of a single row of our array (4 bytes),
+    // sizeof(temptable[0][0]) represents the size of a single entity in the array (2 bytes),
+    //
+    
+    int numRows = sizeof(fanCurve)/sizeof(fanCurve[0]);
+    // int numCols = sizeof(temptable[0])/sizeof(temptable[0][0]);
+    // Serial.print("Number of rows = ");Serial.println(numRows);
+    // Serial.print("Number of cols = ");Serial.println(numCols);
+
+    return linearInterpolate(temp, fanCurve, numRows);
+}
+
+// Stolen from Heater.ino
+/**
+ * Linear interpolate a value using a table for lookup.
+ * @param {double} x - X value to interpolate to.
+ * @param {short} table - Table containing lookup values.
+ * @param {int} numRows - Number of rows in table.
+ */
+float linearInterpolate(double x, const short table[][2], int numRows) {
+    // numRows must be submitted as an argument since table is a pointer
+    // therefore sizeof(table) will always be 2, causing errors
+    
+    float x0, x1, y0, y1, y;
+
+    // TODO: Add code that if a value doesn't match (1023 or 0), proper flags are raised
+
+    for (int i = 0; i < numRows; i++) { // for each row in table
+        if (x > table[i][0] && x < table[i + 1][0]) {
+            x0 = table[i][0];       // lower bound x
+            x1 = table[i + 1][0];   // upper bound x
+            y0 = table[i][1];       // lower bound y
+            y1 = table[i + 1][1];   // upper bound y
+        }
+    }
+
+    // Serial.print("x0: ");Serial.print(x0);
+    // Serial.print(" y0: ");Serial.println(y0);
+    // Serial.print("x1: ");Serial.print(x1);
+    // Serial.print(" y1: ");Serial.println(y1);
+
+    return y0 + ((y1 - y0) / (x1 - x0)) * (x - x0);
+
 }
