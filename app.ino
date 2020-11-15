f9287d4d82dec7a03ac2821cada5e577d5e4c41e
EnUfor <EnUfor@gmail.com>
2020-11-14 23:56:45 -0500

    Separation of ESP0 class

--- app.ino
+++ app.ino
@@ -5,127 +5,106 @@
 #include <PID_v1.h>
 #include "config.h"     // Turns out you need to use quotes for relative imports (2 hrs wasted)
 // #include "fanCurve.h"   // Also turns out the includes' first character has to be lower case (10 min wasted)
-#include "Classes/rack.h"
-Rack rack;
+// #include "Classes/rack.h"
 
-class ESP0
-{
-private:
 
-    double Kp = 9.0;
-    double Ki = 2.0;
-    double Kd = 0.5;
+#include "Classes/ESP0.h"
+// class ESP0
+// {
+// private:
 
-    WiFiClient wificlient;
+//     double Kp = 9.0;
+//     double Ki = 2.0;
+//     double Kd = 0.5;
 
-    // Connects ESP8266 to Wi-Fi
-    void connect_wifi() {
-        WiFi.begin(SSID, WIFI_PASSWORD);
-        Serial.println((String)"Connecting to SSID: " + SSID);
+//     WiFiClient wificlient;
 
-        int i = 0;
-        double continueDelay = millis();
-        while (WiFi.status() != WL_CONNECTED && ((millis() - continueDelay) < 8000)) {
-            delay(1000);
-            Serial.print((String)++i + "...");
-        }
-        Serial.println('\n');
+//     String clientId;
 
-        if (WiFi.status() == WL_CONNECTED) {
-            Serial.println((String)"Connected to SSID: " + SSID);
-            // Serial.println((String)"IP Address: " + (String)WiFi.localIP());
-            Serial.print("IP Address: "); Serial.print(WiFi.localIP());
-        } else {
-            Serial.println((String)"Failed to connect to WiFi with state: " + WiFi.status());
-        }
-        Serial.println('\n');
-    }
+//     // Connects ESP8266 to Wi-Fi
+//     void connect_wifi() {
+//         WiFi.begin(SSID, WIFI_PASSWORD);
+//         Serial.println((String)"Connecting to SSID: " + SSID);
 
-public:
-    ESP0() {};
+//         int i = 0;
+//         double continueDelay = millis();
+//         while (WiFi.status() != WL_CONNECTED && ((millis() - continueDelay) < 8000)) {
+//             delay(1000);
+//             Serial.print((String)++i + "...");
+//         }
+//         Serial.println('\n');
 
-    double setTemp, currentTemp, pidSpeed;
-    PID pid = PID(&currentTemp, &pidSpeed, &setTemp, Kp, Ki, Kd, REVERSE);    // Reverse since we're cooling
+//         if (WiFi.status() == WL_CONNECTED) {
+//             Serial.println((String)"Connected to SSID: " + SSID);
+//             // Serial.println((String)"IP Address: " + (String)WiFi.localIP());
+//             Serial.print("IP Address: "); Serial.print(WiFi.localIP());
+//         } else {
+//             Serial.println((String)"Failed to connect to WiFi with state: " + WiFi.status());
+//         }
+//         Serial.println('\n');
+//     }
 
-    String clientId;
+// public:
+//     ESP0() {};
 
-    PubSubClient client = PubSubClient(wificlient);
+//     double setTemp, currentTemp, pidSpeed;
+//     PID pid = PID(&currentTemp, &pidSpeed, &setTemp, Kp, Ki, Kd, REVERSE);    // Reverse since we're cooling
 
-    // Setup routine (not run on class declaration)
-    void setup() {
-        pid.SetOutputLimits(0, 100);
-        pid.SetMode(AUTOMATIC);
+//     PubSubClient client = PubSubClient(wificlient);
 
-        clientId = "ESP-";
-        clientId += system_get_chip_id();
-        Serial.println((String)"clientId: " + clientId);
+//     // Setup routine (not run on class declaration)
+//     void setup() {
+//         pid.SetOutputLimits(0, 100);
+//         pid.SetMode(AUTOMATIC);
 
-        connect_wifi();
+//         clientId = "ESP-";
+//         clientId += system_get_chip_id();
+//         Serial.println((String)"clientId: " + clientId);
 
-        // Initialize MQTT client
-        client.setServer(MQTT_SERVER, MQTT_PORT);
-        // client.setCallback(std::bind( &ESP0::callback));       // Run callback function for when MQTT message received
+//         connect_wifi();
 
-        // client.setCallback(callback);
+//         // Initialize MQTT client
+//         client.setServer(MQTT_SERVER, MQTT_PORT);
+//         // callback set before setup is run
 
-        client.subscribe(SUB_MAN_FAN);  // Sub first to get state
-        client.subscribe(SUB_INLET_FAN);
-        client.subscribe(SUB_OUTLET_FAN);
-    }
+//         connect_MQTT();
 
-    void loop() {
-        //Loop here
-    }
+//         client.subscribe(SUB_MAN_FAN);  // Sub first to get state
+//         client.subscribe(SUB_INLET_FAN);
+//         client.subscribe(SUB_OUTLET_FAN);
+//     }
 
-    // Connects to MQTT broker
-    void connect_MQTT() {
-        Serial.println((String)"Connecting to MQTT: " + MQTT_SERVER);
-        if (client.connect(clientId.c_str(), MQTT_USER, MQTT_PASSWORD)) {
-            Serial.println("Connected to MQTT");
-        } else {
-            Serial.println((String)"Failed to connect to MQTT with state: " + client.state());
-        }
-        Serial.println('\n');
-    }
+//     void loop() {
+//         //Loop here
+//         handleSerial();
+//         client.loop();
+//     }
 
-    void callback(char* topic, uint8_t* payload, unsigned int length) {
-        String topicStr = topic;    // Convert char to String
-        payload[length] = '\0';     // Null terminate
-        int payloadInt = atoi((char*)payload);  // convert payload to int
-        
-        Serial.println((String)"Topic: " + topicStr);
-        Serial.println((String)"Payload: " + payloadInt);
+//     // Connects to MQTT broker
+//     void connect_MQTT() {
+//         Serial.println((String)"Connecting to MQTT: " + MQTT_SERVER);
+//         if (client.connect(clientId.c_str(), MQTT_USER, MQTT_PASSWORD)) {
+//             Serial.println("Connected to MQTT");
+//         } else {
+//             Serial.println((String)"Failed to connect to MQTT with state: " + client.state());
+//         }
+//         Serial.println('\n');
+//     }
 
-        if (topicStr == SUB_MAN_FAN) {
-            rack.manualFans = payloadInt;
-            client.publish(PUB_MAN_FAN_STATE, String(payloadInt).c_str());
-        } else if (topicStr == SUB_INLET_FAN) {
-            if (rack.manualFans)
-            {
-                rack.inlet.setFanSpeed(payloadInt);
-            }    
-        } else if (topicStr == SUB_OUTLET_FAN) {
-            if (rack.manualFans)
-            {
-                rack.outlet.setFanSpeed(payloadInt);
-            }
-        }
-    }
+//     void handleSerial() {
+//     while (Serial.available()) {
+//         char inputBuffer[6];
+//         Serial.readBytesUntil('\n', inputBuffer, 5);
 
-    void handleSerial() {
-    while (Serial.available()) {
-        char inputBuffer[6];
-        Serial.readBytesUntil('\n', inputBuffer, 5);
-
-        int inputFanSpeed = atoi(inputBuffer);
-        rack.setFans(inputFanSpeed);
-        rack.manualFans = false;
-        client.publish(PUB_MAN_FAN_STATE, "0");       // Let HA know it's no longer in control
-        Serial.print((String)"Current PWM = " + inputFanSpeed);
-    }
-    Serial.flush();
-}
-};
+//         int inputFanSpeed = atoi(inputBuffer);
+//         rack.setFans(inputFanSpeed);
+//         rack.manualFans = false;
+//         client.publish(PUB_MAN_FAN_STATE, "0");       // Let HA know it's no longer in control
+//         Serial.print((String)"Current PWM = " + inputFanSpeed);
+//     }
+//     Serial.flush();
+// }
+// };
 
 unsigned long MQTT_sensor_timer;
 unsigned long MQTT_reconnect_timer;
@@ -134,17 +113,6 @@ unsigned long printDelay;
 int MQTT_initial_delay = 10000;
 unsigned int MQTT_delay = MQTT_initial_delay;
 
-// // PID related variables
-double setTemp, currentTemp, pidSpeed;
-double Kp = 9.0; double Ki = 2.0;  double Kd = 0.5;
-
-// PID pid(&currentTemp, &pidSpeed, &setTemp, Kp, Ki, Kd, REVERSE);    // Reverse since we're cooling
-
-// String clientId;
-
-// Initiate WiFi & MQTT
-// WiFiClient wificlient;
-// PubSubClient client(wificlient);
 ESP0 espoooo;
 
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -171,32 +139,12 @@ void callback(char* topic, byte* payload, unsigned int length) {
     }
 }
 
-
-
 void setup() {
     delay(2000);    // delay to ensure serial monitor is connected
     Serial.begin(115200);
 
-    espoooo.setup();
-
     espoooo.client.setCallback(callback);
-    // espoooo.client.setCallback(espoooo->callback);
-
-    // clientId = "ESP-";
-    // clientId += system_get_chip_id();
-    // Serial.println((String)"clientId: " + clientId);
-
-    // connect_wifi();
-
-    // Initialize MQTT client
-    // client.setServer(MQTT_SERVER, MQTT_PORT);
-    // client.setCallback(callback);       // Run callback function for when MQTT message received
-
-    // connect_MQTT();
-
-    // client.subscribe(SUB_MAN_FAN);  // Sub first to get state
-    // client.subscribe(SUB_INLET_FAN);
-    // client.subscribe(SUB_OUTLET_FAN);
+    espoooo.setup();
 
     MQTT_sensor_timer = millis();
     MQTT_reconnect_timer = millis();
@@ -223,15 +171,15 @@ void loop() {
         rack.readSensors();
         // rack.printSensors();
 
-        currentTemp = rack.outlet.temp;
-        setTemp = rack.inlet.temp + 5.0;
+        espoooo.currentTemp = rack.outlet.temp;
+        espoooo.setTemp = rack.inlet.temp + 5.0;
         if (!rack.manualFans) {
             espoooo.pid.Compute();
-            rack.setFans((int)pidSpeed);
+            rack.setFans((int)espoooo.pidSpeed);
         }
         
-        Serial.println((String)"pidSpeed: " + pidSpeed);
-        Serial.println((String)"currentTemp: " + currentTemp);
+        Serial.println((String)"pidSpeed: " + espoooo.pidSpeed);
+        Serial.println((String)"currentTemp: " + espoooo.currentTemp);
 
     }
 
@@ -251,78 +199,5 @@ void loop() {
         }
     }
 
-    espoooo.handleSerial();
-    espoooo.client.loop();
+    espoooo.loop();
 }
-
-// void handleSerial() {
-//     while (Serial.available()) {
-//         char inputBuffer[6];
-//         Serial.readBytesUntil('\n', inputBuffer, 5);
-
-//         int inputFanSpeed = atoi(inputBuffer);
-//         rack.setFans(inputFanSpeed);
-//         rack.manualFans = false;
-//         client.publish(PUB_MAN_FAN_STATE, "0");       // Let HA know it's no longer in control
-//         Serial.print((String)"Current PWM = " + inputFanSpeed);
-//     }
-//     Serial.flush();
-// }
-
-// void callback(char* topic, byte* payload, unsigned int length) {
-//     String topicStr = topic;    // Convert char to String
-//     payload[length] = '\0';     // Null terminate
-//     int payloadInt = atoi((char*)payload);  // convert payload to int
-    
-//     Serial.println((String)"Topic: " + topicStr);
-//     Serial.println((String)"Payload: " + payloadInt);
-
-//     if (topicStr == SUB_MAN_FAN) {
-//         rack.manualFans = payloadInt;
-//         client.publish(PUB_MAN_FAN_STATE, String(payloadInt).c_str());
-//     } else if (topicStr == SUB_INLET_FAN) {
-//         if (rack.manualFans)
-//         {
-//             rack.inlet.setFanSpeed(payloadInt);
-//         }    
-//     } else if (topicStr == SUB_OUTLET_FAN) {
-//         if (rack.manualFans)
-//         {
-//             rack.outlet.setFanSpeed(payloadInt);
-//         }
-//     }
-// }
-
-// // Connects ESP8266 to Wi-Fi
-// void connect_wifi() {
-//     WiFi.begin(SSID, WIFI_PASSWORD);
-//     Serial.println((String)"Connecting to SSID: " + SSID);
-
-//     int i = 0;
-//     double continueDelay = millis();
-//     while (WiFi.status() != WL_CONNECTED && ((millis() - continueDelay) < 8000)) {
-//         delay(1000);
-//         Serial.print((String)++i + "...");
-//     }
-//     Serial.println('\n');
-
-//     if (WiFi.status() == WL_CONNECTED) {
-//         Serial.println((String)"Connected to SSID: " + SSID);
-//         // Serial.println((String)"IP Address: " + (String)WiFi.localIP());
-//         Serial.print("IP Address: "); Serial.print(WiFi.localIP());
-//     } else {
-//         Serial.println((String)"Failed to connect to WiFi with state: " + WiFi.status());
-//     }
-//     Serial.println('\n');
-// }
-
-// // Connects to MQTT broker
-// void connect_MQTT() {
-//     Serial.println((String)"Connecting to MQTT: " + MQTT_SERVER);
-//     if (client.connect(clientId.c_str(), MQTT_USER, MQTT_PASSWORD)) {
-//         Serial.println("Connected to MQTT");
-//     } else {
-//         Serial.println((String)"Failed to connect to MQTT with state: " + client.state());
-//     }
-//     Serial.println('\n');
-// }
