01484e955d6be44c05c9284986853a68764011ab
EnUfor <EnUfor@gmail.com>
2020-11-15 23:45:30 -0500

    General cleanup. Fixing pubsub errors

--- app.ino
+++ app.ino
@@ -1,110 +1,13 @@
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <PubSubClient.h>
+// #include <ESP8266WiFi.h>
+// #include <PubSubClient.h>
 #include <Wire.h>
-#include <PID_v1.h>
+// #include <PID_v1.h>
 #include "config.h"     // Turns out you need to use quotes for relative imports (2 hrs wasted)
 // #include "fanCurve.h"   // Also turns out the includes' first character has to be lower case (10 min wasted)
 // #include "Classes/rack.h"
-
-
 #include "Classes/ESP0.h"
-// class ESP0
-// {
-// private:
 
-//     double Kp = 9.0;
-//     double Ki = 2.0;
-//     double Kd = 0.5;
-
-//     WiFiClient wificlient;
-
-//     String clientId;
-
-//     // Connects ESP8266 to Wi-Fi
-//     void connect_wifi() {
-//         WiFi.begin(SSID, WIFI_PASSWORD);
-//         Serial.println((String)"Connecting to SSID: " + SSID);
-
-//         int i = 0;
-//         double continueDelay = millis();
-//         while (WiFi.status() != WL_CONNECTED && ((millis() - continueDelay) < 8000)) {
-//             delay(1000);
-//             Serial.print((String)++i + "...");
-//         }
-//         Serial.println('\n');
-
-//         if (WiFi.status() == WL_CONNECTED) {
-//             Serial.println((String)"Connected to SSID: " + SSID);
-//             // Serial.println((String)"IP Address: " + (String)WiFi.localIP());
-//             Serial.print("IP Address: "); Serial.print(WiFi.localIP());
-//         } else {
-//             Serial.println((String)"Failed to connect to WiFi with state: " + WiFi.status());
-//         }
-//         Serial.println('\n');
-//     }
-
-// public:
-//     ESP0() {};
-
-//     double setTemp, currentTemp, pidSpeed;
-//     PID pid = PID(&currentTemp, &pidSpeed, &setTemp, Kp, Ki, Kd, REVERSE);    // Reverse since we're cooling
-
-//     PubSubClient client = PubSubClient(wificlient);
-
-//     // Setup routine (not run on class declaration)
-//     void setup() {
-//         pid.SetOutputLimits(0, 100);
-//         pid.SetMode(AUTOMATIC);
-
-//         clientId = "ESP-";
-//         clientId += system_get_chip_id();
-//         Serial.println((String)"clientId: " + clientId);
-
-//         connect_wifi();
-
-//         // Initialize MQTT client
-//         client.setServer(MQTT_SERVER, MQTT_PORT);
-//         // callback set before setup is run
-
-//         connect_MQTT();
-
-//         client.subscribe(SUB_MAN_FAN);  // Sub first to get state
-//         client.subscribe(SUB_INLET_FAN);
-//         client.subscribe(SUB_OUTLET_FAN);
-//     }
-
-//     void loop() {
-//         //Loop here
-//         handleSerial();
-//         client.loop();
-//     }
-
-//     // Connects to MQTT broker
-//     void connect_MQTT() {
-//         Serial.println((String)"Connecting to MQTT: " + MQTT_SERVER);
-//         if (client.connect(clientId.c_str(), MQTT_USER, MQTT_PASSWORD)) {
-//             Serial.println("Connected to MQTT");
-//         } else {
-//             Serial.println((String)"Failed to connect to MQTT with state: " + client.state());
-//         }
-//         Serial.println('\n');
-//     }
-
-//     void handleSerial() {
-//     while (Serial.available()) {
-//         char inputBuffer[6];
-//         Serial.readBytesUntil('\n', inputBuffer, 5);
-
-//         int inputFanSpeed = atoi(inputBuffer);
-//         rack.setFans(inputFanSpeed);
-//         rack.manualFans = false;
-//         client.publish(PUB_MAN_FAN_STATE, "0");       // Let HA know it's no longer in control
-//         Serial.print((String)"Current PWM = " + inputFanSpeed);
-//     }
-//     Serial.flush();
-// }
-// };
 
 unsigned long MQTT_sensor_timer;
 unsigned long MQTT_reconnect_timer;
