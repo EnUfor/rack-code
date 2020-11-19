c5fda028219b1ff29e4b5ece05a6e4860936380f
EnUfor <EnUfor@gmail.com>
2020-11-18 23:32:08 -0500

    rename of ESP0 instance

--- app.ino
+++ app.ino
@@ -12,7 +12,7 @@ unsigned long printDelay;
 int MQTT_initial_delay = 10000;
 unsigned int MQTT_delay = MQTT_initial_delay;
 
-ESP0 espoooo;
+ESP0 esp0;
 
 void callback(char* topic, byte* payload, unsigned int length) {
     String topicStr = topic;    // Convert char to String
@@ -24,7 +24,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
     if (topicStr == SUB_MAN_FAN) {
         rack.manualFans = payloadInt;
-        espoooo.client.publish(PUB_MAN_FAN_STATE, String(payloadInt).c_str());
+        esp0.client.publish(PUB_MAN_FAN_STATE, String(payloadInt).c_str());
     } else if (topicStr == SUB_INLET_FAN) {
         if (rack.manualFans)
         {
@@ -42,8 +42,8 @@ void setup() {
     delay(2000);    // delay to ensure serial monitor is connected
     Serial.begin(115200);
 
-    espoooo.client.setCallback(callback);
-    espoooo.setup();
+    esp0.client.setCallback(callback);
+    esp0.setup();
 
     MQTT_sensor_timer = millis();
     MQTT_reconnect_timer = millis();
@@ -52,10 +52,10 @@ void setup() {
 
 void loop() {
     // If WiFi is connected, MQTT is not connected, and delay is satisfied: reconnect MQTT
-    if (WiFi.status() == WL_CONNECTED && !espoooo.client.connected() && (millis() - MQTT_reconnect_timer >= MQTT_delay)) {
+    if (WiFi.status() == WL_CONNECTED && !esp0.client.connected() && (millis() - MQTT_reconnect_timer >= MQTT_delay)) {
         MQTT_reconnect_timer = millis();
-        espoooo.connect_MQTT();
-        if (!espoooo.client.connected() && MQTT_delay < 60000)  // IF not successful and delay is less than 60 seconds
+        esp0.connect_MQTT();
+        if (!esp0.client.connected() && MQTT_delay < 60000)  // IF not successful and delay is less than 60 seconds
         {
             MQTT_delay += 10000;    // Increase delay to avoid unecessary reconnect tries
         } else
@@ -70,15 +70,15 @@ void loop() {
         rack.readSensors();
         // rack.printSensors();
 
-        espoooo.currentTemp = rack.outlet.temp;
-        espoooo.setTemp = rack.inlet.temp + 5.0;
+        esp0.currentTemp = rack.outlet.temp;
+        esp0.setTemp = rack.inlet.temp + 5.0;
         if (!rack.manualFans) {
-            espoooo.pid.Compute();
-            rack.setFans((int)espoooo.pidSpeed);
+            esp0.pid.Compute();
+            rack.setFans((int)esp0.pidSpeed);
         }
         
-        Serial.println((String)"pidSpeed: " + espoooo.pidSpeed);
-        Serial.println((String)"currentTemp: " + espoooo.currentTemp);
+        Serial.println((String)"pidSpeed: " + esp0.pidSpeed);
+        Serial.println((String)"currentTemp: " + esp0.currentTemp);
 
     }
 
@@ -87,16 +87,16 @@ void loop() {
     {
         MQTT_sensor_timer = millis();
 
-        espoooo.client.publish(PUB_INLET_TEMP, String(rack.inlet.temp).c_str());
-        espoooo.client.publish(PUB_INLET_HUMID, String(rack.inlet.humidity).c_str());
-        espoooo.client.publish(PUB_OUTLET_TEMP, String(rack.outlet.temp).c_str());
-        espoooo.client.publish(PUB_OUTLET_HUMID, String(rack.outlet.humidity).c_str());
+        esp0.client.publish(PUB_INLET_TEMP, String(rack.inlet.temp).c_str());
+        esp0.client.publish(PUB_INLET_HUMID, String(rack.inlet.humidity).c_str());
+        esp0.client.publish(PUB_OUTLET_TEMP, String(rack.outlet.temp).c_str());
+        esp0.client.publish(PUB_OUTLET_HUMID, String(rack.outlet.humidity).c_str());
 
         if(!rack.manualFans) {
-            espoooo.client.publish(PUB_INLET_FAN, String(rack.inlet.fanSpeed).c_str());
-            espoooo.client.publish(PUB_OUTLET_FAN, String(rack.outlet.fanSpeed).c_str());
+            esp0.client.publish(PUB_INLET_FAN, String(rack.inlet.fanSpeed).c_str());
+            esp0.client.publish(PUB_OUTLET_FAN, String(rack.outlet.fanSpeed).c_str());
         }
     }
 
-    espoooo.loop();
+    esp0.loop();
 }
