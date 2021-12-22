4e6fdbd011f03742e4cbdfebc29523e30e974837
EnUfor <EnUfor@gmail.com>
2021-12-22 02:19:20 -0500

    Migrates to headers files for rack class

--- rack.h
+++ rack.h
@@ -1,47 +1,16 @@
-#include <cactus_io_BME280_I2C.h>
-#include <CircularBuffer.h>
-#include "pins.h"
+#ifndef _Rack2_h_
 
-#include "zone.h"
+#define _Rack2_h_
 
-class Rack
+class Rack2
 {
 private:
-    /**
-     * Read sensor and set respective values
-     * Must be passed by reference (3 hrs wasted)
-    **/    
-    void reader(Zone& zone) {
-        if (zone.online) {
-            zone.sensor.readSensor();
-            zone.temp = zone.sensor.getTemperature_F();
-            zone.humidity = zone.sensor.getHumidity();
-        }
-    }
+    void setup();
+    void reader(Zone& zone);
+    void printer(String name, Zone zone);
 
-    /**
-     * Print sensor values to serial
-    **/ 
-    void printer(String name, Zone zone) {
-        Serial.print(name); Serial.print(":\t");
-        if (zone.online) {
-            Serial.print(zone.temp); Serial.print(" *F\t\t");
-            Serial.print(zone.humidity); Serial.println(" %");
-        } else
-        {
-            Serial.println("Not Connected");
-        }
-    }
-
-    void setup() {
-        pinMode(FANPOWER, OUTPUT);
-        pinMode(LEDPIN, OUTPUT);
-        setFans(100);       // Set fans to 100% on class declaration
-    }
 public:
-    Rack() {
-        setup();
-    };
+    Rack2();
 
     // NodeMCU ESP8266 pinout: SCL = 5 (D1) SDA = 4 (D2)
     Zone inlet = {0x76, INLETFANPIN};
@@ -49,49 +18,13 @@ public:
 
     CircularBuffer<double, 20> inletHistory;
     CircularBuffer<double, 20> outletHistory;
-    
+
     bool manualFans = false;
 
-    /**
-     * Read sensor values
-    **/
-    void readSensors() {
-        reader(inlet);
-        reader(outlet);
-        inletHistory.push(inlet.temp);
-        outletHistory.push(outlet.temp);
-    }
-
-    /**
-     * Print sensor values
-    **/
-    void printSensors() {
-        printer("Inlet", inlet);
-        printer("Outlet", outlet);
-        Serial.println("\n");
-    }
-
-    /**
-     * Print Buffer Values
-    **/
-    void printBuff(CircularBuffer<double, 20> & buff) {
-        for (int i = buff.size() - 1; i >= 0; i--)
-        {
-            Serial.print(buff.operator[](i));
-            if (i > 0)
-            {
-                Serial.print(", ");
-            }    
-        }
-        Serial.print("\n");
-    }
-
-    /**
-     * Sets both fan speeds "simultaneously"
-    **/ 
-    void setFans(int speed) {
-        // Serial.println((String)"setFans Ran: " + speed);
-        inlet.setFanSpeed(speed);
-        outlet.setFanSpeed(speed);
-    }
+    void readSensors();
+    void printSensors();
+    void printBuff(CircularBuffer<double, 20> & buff);
+    void setFans(int speed);
 };
+
+#endif
